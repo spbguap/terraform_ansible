d259c1b4f9f705342b1dc835188048acd186b266
Dmitrii Busel <spbguap@yandex.com>
2025-10-07 17:34:10 +0000

    Revert "Asible and Terraform config"
    This reverts commit 1896008e6ac93e76ab7af73b629c10abfc5356e0.

--- terraform/output.tf
+++
@@ -1,7 +0,0 @@
-output "vm_ip" {
-  value = { for k, v in  yandex_compute_instance.virtual_machine : k => v.network_interface.0.ip_address }
-}
-
-output "vm_nat_ip" {
-  value = { for k, v in  yandex_compute_instance.virtual_machine : k => v.network_interface.0.nat_ip_address}
-}
--- terraform/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "virtual_machines" {
- default = ""
-}
